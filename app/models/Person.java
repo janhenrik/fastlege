b7b670a7135680968b816644e8a8e7bff93ca89e
Jan Henrik Gundelsby <janhenrik@gundelsby.com>
2011-10-24 12:50:46 +0200

    Working facebook connect for email

--- app/models/Person.java
+++ app/models/Person.java
@@ -1,6 +1,8 @@
 package models;
 
+import com.google.gson.JsonObject;
 import play.db.jpa.Model;
+import play.mvc.Scope;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
@@ -12,6 +14,16 @@ public class Person extends Model {
     public String navn;
     public String epost;
     @OneToMany
-    public List <FastlegeOnske> fastlegeOnsker;
+    public List<FastlegeOnske> fastlegeOnsker;
 
+    public static void facebookOAuthCallback(JsonObject data) {
+        String epost = data.get("email").getAsString();
+        Person person = find("epost=?", epost).first();
+        if (person == null) {
+            person = new Person();
+            person.epost = epost;
+            person.save();
+        }
+        Scope.Session.current().put("user", person.epost);
+    }
 }
